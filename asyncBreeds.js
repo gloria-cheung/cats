3318e17598c132ceba84e42df45b4e8ce3706abb
Gloria <gloriacheung812@gmail.com>
2022-06-18 17:54:18 +0000

    export for testing and comment out manual testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -24,13 +24,16 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
     if (!error) functionToRunWhenThingsAreDone(data);
+    if (error) functionToRunWhenThingsAreDone(undefined);
   });
 };
 
-// CHANGE 1: Moved the console.log into a new function:
-const printOutCatBreed = breed => {
-  console.log('Return Value: ', breed) // => print out details correctly.
-};
+// // CHANGE 1: Moved the console.log into a new function:
+// const printOutCatBreed = breed => {
+//   console.log('Return Value: ', breed) // => print out details correctly.
+// };
 
-// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile('Bombay', printOutCatBreed);
+// // CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+// breedDetailsFromFile('Bombay', printOutCatBreed);
+
+module.exports = breedDetailsFromFile
